3126464329b4c0b97c52be19b02fcc4317bae165
wmp666 <2134868121@qq.com>
2026-07-31 17:14:56 +0800

    将多个翻译文件统一为一个

--- src/com/wmp/downloader/Run.java
+++ src/com/wmp/downloader/Run.java
@@ -12,7 +12,7 @@ import java.awt.*;
 public class Run {
     private static final Logger logger = Logger.getLogger(Run.class);
 
-    public static final String VERSION = "0.2.2.1";
+    public static final String VERSION = "0.2.3";
 
     static void main(String[] args) {
         DataControl.load();
@@ -35,6 +35,9 @@ public class Run {
             JPanel panel = new JPanel(new BorderLayout());
             JTextArea textArea = new JTextArea();
             textArea.setText("""
+                    0.2.3
+                    1.将翻译文件统一为一个文件
+                    
                     0.2.2
                     1.增加新的设置项，个性化开放度更高
                     2.整顿部分UI
